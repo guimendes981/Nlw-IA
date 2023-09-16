968385c6f417bd994db3f754a15ab98e9bdff80c
Guilherme Mendes Barnabe <guilherme.barnabe@grupointegrado.br>
2023-09-16 13:44:54 -0300

    Ajustes

--- server/download.js
+++ server/download.js
@@ -2,14 +2,25 @@ import ytdl from "ytdl-core"
 import fs from "fs"
 
 export const download = (videoId) => {
-  const videoUrl = "https://www.youtube.com/watch?=" + videoId
-  console.log("Realizando download do vídeo..." + videoId)
+  const videoURL = "https://www.youtube.com/shorts/" + videoId
+  console.log("Realizando o download do vídeo:", videoId)
 
-  ytdl(videoUrl, { quality: "lowestaudio", filter: "audioonly" }).on(
-    "info",
-    (info) => {
-      const seconds = info.formats[0].aproxDurationMs / 1000
-      console.log(seconds)
-    }
-  )
+  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+    .on("info", (info) => {
+      const seconds = info.formats[0].approxDurationMs / 1000
+
+      if (seconds > 60) {
+        throw new Error("A duração desse vídeo é maior do que 60 segundos.")
+      }
+    })
+    .on("end", () => {
+      console.log("Download do vídeo finalizado.")
+    })
+    .on("error", (error) => {
+      console.log(
+        "Não foi possível fazer o download do vídeo. Detalhes do erro:",
+        error
+      )
+    })
+    .pipe(fs.createWriteStream("./tmp/audio.mp4"))
 }
--- server/index.js
+++ server/index.js
@@ -3,14 +3,13 @@ import express from "express"
 
 import { download } from "./download.js"
 
-const app = express() //criando app express
-app.use(cors()) //habilitando cors
+const app = express()
+app.use(cors())
 
 app.get("/summary/:id", (request, response) => {
   download(request.params.id)
-  response.send("Id do vídeo" + request.params.id)
+
+  response.json({ result: "Download do vídeo realizado com sucesso!" })
 })
 
-app.listen(3333, () => {
-  console.log("Server started on port 3333!")
-})
+app.listen(3333, () => console.log("Server is running on port 3333"))
